80f8e36fa0e47ab6e2b8d8c4f67e8249babf9936
namelessly <1650345422@qq.com>
2025-04-05 19:16:14 +0800

    lab4 message

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,20 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace(void)
+{
+  printf("backtrace:\n");
+  
+  uint64 fp = r_fp();
+  uint64 top = PGROUNDUP(fp);
+  uint64 bottom = PGROUNDDOWN(fp);
+  
+  // 遍历栈帧
+  while(fp >= bottom && fp < top) {
+    uint64 ra = *(uint64*)(fp - 8);  // 返回地址在 fp-8
+    printf("%p\n", ra);
+    fp = *(uint64*)(fp - 16);  // 上一个帧指针在 fp-16
+  }
+}
