fbc0e1005fdb7ec0d23af8ef71d6675d1f8bb89d
namelessly <1650345422@qq.com>
2025-04-13 11:01:23 +0800

    lab7 message

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,15 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t locks[NBUCKET]; // one lock per bucket
+
+void init_locks() {
+  // 在main函数一开始 呼叫一下这个函数
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&locks[i], NULL);
+  }
+}
+
 
 double
 now()
@@ -39,7 +48,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&locks[i]); // 加bucket锁
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -53,19 +62,21 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&locks[i]); // 放bucket锁
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&locks[i]);  // 加bucket锁
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
 
+  pthread_mutex_unlock(&locks[i]); // 放bucket锁
   return e;
 }
 
@@ -99,6 +110,7 @@ get_thread(void *xa)
 int
 main(int argc, char *argv[])
 {
+  init_locks();
   pthread_t *tha;
   void *value;
   double t1, t0;
